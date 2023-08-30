4cfe642846e3353b603dca1a76f630baa024bcf7
zhouxk1204 <346960620@qq.com>
2023-08-30 10:51:00 +0800

    init vue3 with tailwind css

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p-4 bg-gray-100">
+    <h1 class="text-2xl font-bold">Hello Tailwind CSS</h1>
+    <p class="text-gray-600">This is a Vue 3 project using Tailwind CSS.</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
