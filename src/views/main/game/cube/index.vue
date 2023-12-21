7262f4c42da989964bc8392a0b2fe22fefc3b050
zhouxk1204 <346960620@qq.com>
2023-12-21 16:46:24 +0800

    update cube

--- src/views/main/game/cube/index.vue
+++ src/views/main/game/cube/index.vue
@@ -1,98 +1,137 @@
 <template>
-  <div class="rubiks-cube">
-    <div class="face" v-for="(face, index) in cube.faces" :key="index">
-      <div v-for="(row, rowIndex) in face" :key="rowIndex" class="row">
+  <div class="flex items-center justify-center w-full h-full perspective">
+    <div
+      class="relative preserve-3d backface-hidden"
+      :style="{
+        transform: `rotateX(-24deg) rotateY(53deg) rotateZ(0deg)`,
+      }"
+    >
+      <div
+        class="absolute top-0 left-0 preserve-3d backface-hidden rounded-2xl"
+        v-for="block in blocks"
+        :style="{
+          width: `${blockSize}px`,
+          height: `${blockSize}px`,
+          transform: `translateX(${block.x * blockSize}px) translateY(${
+            block.y * blockSize
+          }px) translateZ(${-block.z * blockSize}px)`,
+        }"
+      >
         <div
-          v-for="(color, colIndex) in row"
-          :key="colIndex"
-          class="cubelet"
-          :style="{ backgroundColor: color }"
-        ></div>
+          v-for="area in block.areas"
+          class="block-shadow absolute top-0 left-0 w-full h-full p-[10px] overflow-hidden bg-[#0a1019] rounded-xl"
+          :class="area.direct"
+          :style="{
+            transform: area.transform,
+          }"
+        >
+          <div
+            :style="{ 'background-color': area.background }"
+            class="w-full h-full shadow-2xl rounded-2xl"
+          ></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+const order = 3;
+const blockSize = 100;
 
-export default defineComponent({
-  name: "RubiksCube",
-  setup() {
-    // Initial state of the Rubik's Cube
-    const cube = ref({
-      faces: [
-        [
-          ["#ff0000", "#ff0000", "#ff0000"],
-          ["#ff0000", "#ff0000", "#ff0000"],
-          ["#ff0000", "#ff0000", "#ff0000"],
-        ],
-        [
-          ["#00ff00", "#00ff00", "#00ff00"],
-          ["#00ff00", "#00ff00", "#00ff00"],
-          ["#00ff00", "#00ff00", "#00ff00"],
-        ],
-        [
-          ["#0000ff", "#0000ff", "#0000ff"],
-          ["#0000ff", "#0000ff", "#0000ff"],
-          ["#0000ff", "#0000ff", "#0000ff"],
-        ],
-        [
-          ["#ffff00", "#ffff00", "#ffff00"],
-          ["#ffff00", "#ffff00", "#ffff00"],
-          ["#ffff00", "#ffff00", "#ffff00"],
-        ],
-        [
-          ["#ff6600", "#ff6600", "#ff6600"],
-          ["#ff6600", "#ff6600", "#ff6600"],
-          ["#ff6600", "#ff6600", "#ff6600"],
-        ],
-        [
-          ["#cccccc", "#cccccc", "#cccccc"],
-          ["#cccccc", "#cccccc", "#cccccc"],
-          ["#cccccc", "#cccccc", "#cccccc"],
-        ],
-      ],
-    });
+const getBlockAreas = (x: number, y: number, z: number) => {
+  const areas = [];
+  if (x === 0) {
+    areas.push(getBlockStyle("Left"));
+  }
+  if (x === order - 1) {
+    areas.push(getBlockStyle("Right"));
+  }
+  if (y === 0) {
+    areas.push(getBlockStyle("Up"));
+  }
+  if (y === order - 1) {
+    areas.push(getBlockStyle("Down"));
+  }
+  if (z === 0) {
+    areas.push(getBlockStyle("Front"));
+  }
+  if (z === order - 1) {
+    areas.push(getBlockStyle("Back"));
+  }
+  return areas;
+};
 
-    return {
-      cube,
-    };
-  },
-});
+const getBlockStyle = (direct: string) => {
+  const transforms: any = {
+    Up: `translateY(-${blockSize}px) rotateX(90deg) `,
+    Down: `translateY(${blockSize}px) rotateX(-90deg)`,
+    Left: `translateX(-${blockSize}px) rotateY(-90deg)`,
+    Right: `translateX(-${blockSize}px) rotateY(90deg) translateZ(200px)`,
+    Front: `translateZ(0)`,
+    Back: `translateZ(-${blockSize}px)`,
+  };
+  const background: any = {
+    Up: "#f1f1f1",
+    Down: "#d17f30",
+    Left: "#7fad46",
+    Right: "#4a8399",
+    Front: "#bf402c",
+    Back: "#f9ec67",
+  };
+  return {
+    direct,
+    transform: transforms[direct],
+    background: background[direct],
+  };
+};
+
+const createCube = () => {
+  const blocks = [];
+  for (let x = 0; x < order; x++) {
+    for (let y = 0; y < order; y++) {
+      for (let z = 0; z < order; z++) {
+        blocks.push({
+          x,
+          y,
+          z,
+          areas: getBlockAreas(x, y, z),
+        });
+      }
+    }
+  }
+  return blocks;
+};
+const blocks = createCube();
 </script>
+<style lang="scss" scoped>
+.perspective {
+  perspective: 1200px;
+}
 
-<style scoped>
-.rubiks-cube {
-  width: 160px;
-  height: 160px;
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  perspective: 800px;
+.preserve-3d {
   transform-style: preserve-3d;
 }
 
-.face {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: grid;
-  grid-column: 3;
-  grid-row: 3;
-  transform-style: preserve-3d;
+.backface-hidden {
+  backface-visibility: hidden;
+  background-color: #0a1019;
 }
 
-.row {
-  display: flex;
-  flex: 1;
+.Up {
+  transform-origin: bottom;
+}
+.Down {
+  transform-origin: top;
+}
+.Left {
+  transform-origin: right;
+}
+.Right {
+  transform-origin: left;
 }
 
-.cubelet {
-  flex: 1;
-  margin: 2px;
-  box-sizing: border-box;
+.block-shadow {
+  box-shadow: 0 0 2px 4px #0a1019;
 }
 </style>
