9d630e991a266cdfb81d6160f7eca250ca5b226b
zhouxk1204 <346960620@qq.com>
2024-01-23 11:33:34 +0800

    update icon

--- src/views/main/home/index.vue
+++ src/views/main/home/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <Typewriter class="text-4xl"></Typewriter>
+    <!-- <Typewriter class="text-4xl"></Typewriter>
     <FillUp text="Love to the depths, is lonely." class="text-7xl"></FillUp>
-    <StepFadeIn :text="text"></StepFadeIn>
-    <el-button type="primary" :icon="Edit" circle />
+    <StepFadeIn :text="text"></StepFadeIn> -->
+    <i-ep-document />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Edit } from "@element-plus/icons-vue";
 const text =
   "There were a sensitivity and a beauty to her that have nothing to do with looks. She was one to be listened to, whose words were so easy to take to heart.";
 </script>
