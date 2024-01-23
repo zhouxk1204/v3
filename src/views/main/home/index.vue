fc2bd58c393ec9e909905d19028316fbccca37d8
zhouxk1204 <346960620@qq.com>
2024-01-23 11:11:55 +0800

    update element plus

--- src/views/main/home/index.vue
+++ src/views/main/home/index.vue
@@ -3,10 +3,12 @@
     <Typewriter class="text-4xl"></Typewriter>
     <FillUp text="Love to the depths, is lonely." class="text-7xl"></FillUp>
     <StepFadeIn :text="text"></StepFadeIn>
+    <el-button type="primary" :icon="Edit" circle />
   </div>
 </template>
 
 <script setup lang="ts">
+import { Edit } from "@element-plus/icons-vue";
 const text =
   "There were a sensitivity and a beauty to her that have nothing to do with looks. She was one to be listened to, whose words were so easy to take to heart.";
 </script>
