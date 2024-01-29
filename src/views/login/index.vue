508c3bfd29c398039fccc95443417ebdda4da074
zhouxk1204 <346960620@qq.com>
2024-01-29 14:31:14 +0800

    update login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div
-    class="relative flex items-center justify-center w-screen h-screen overflow-hidden bg-bottom bg-cover bg-transition"
-    :style="{ backgroundImage: `url(${backgroundImage})` }"
+    class="relative w-screen h-screen overflow-hidden bg-bottom bg-cover bg-transition"
+    :style="{
+      backgroundImage: `url(https://picsum.photos/1920/1080.webp)`,
+    }"
   >
     <el-form
       ref="formRef"
       :model="loginForm"
       size="large"
       :rules="rules"
-      class="w-full p-10 pb-8 bg-white rounded-md shadow-2xl bg-opacity-90 xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-full"
+      class="absolute w-full p-10 -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-2xl bg-opacity-90 xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-full left-1/2 top-1/2"
     >
       <h1 class="pb-2 mb-6 text-4xl text-center">🍑</h1>
       <el-form-item prop="email">
@@ -25,21 +27,12 @@
         />
       </el-form-item>
       <el-button
-        class="w-full mb-3"
+        class="w-full"
         type="primary"
         @click="submitForm"
         native-type="submit"
         >注册 / 登录
       </el-button>
-      <div class="flex justify-center">
-        <el-button
-          link
-          :loading="isLoading"
-          @click="onNextBg"
-          native-type="button"
-          >不喜欢这张背景图，换一张?
-        </el-button>
-      </div>
     </el-form>
   </div>
 </template>
@@ -91,33 +84,5 @@ const submitForm = () => {
       console.log("error submit!", fields);
     }
   });
-};
-
-const getBackgroundImages = () => {
-  return Object.keys(
-    import.meta.glob("/src/assets/img/bg/*.{png,jpg,gif,svg,avif}")
-  ).map((e) => new URL(e, import.meta.url).href);
-};
-
-const getRandomBackgroundImage = (): string => {
-  const images = getBackgroundImages();
-  const index = Math.floor(Math.random() * images.length);
-  return images[index];
-};
-
-const backgroundImage = ref(getRandomBackgroundImage());
-
-const isLoading = ref(false);
-const onNextBg = () => {
-  isLoading.value = true;
-  const newBg = getRandomBackgroundImage();
-  if (backgroundImage.value !== newBg) {
-    backgroundImage.value = newBg;
-    setTimeout(() => {
-      isLoading.value = false;
-    }, 3000);
-  } else {
-    onNextBg();
-  }
 };
 </script>
