e8c6e22569c736d389fb57c43a47f7179364e4ed
周小康 <346960620@qq.com>
2025-01-06 20:42:44 +0800

    update ai

--- src/views/ai/index.vue
+++ src/views/ai/index.vue
@@ -1,19 +1,48 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col gap-1 p-3  bg-[#f1f1f1] rounded-2xl max-w-[768px] w-full">
-      <textarea v-model="question" class="p-2 bg-[#f1f1f1] outline-none resize-none"
-        placeholder="给DeepSeek发送消息"></textarea>
-      <div class="flex justify-end">
-        <button :class="question.length === 0 ? 'text-[#f1f1f1] bg-[#cecece]' : 'text-white bg-black'"
-          class="w-8 h-8 overflow-hidden text-white bg-black rounded-full" :disabled="question.length === 0">
-          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"
-            class="icon-2xl">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-              d="M15.1918 8.90615C15.6381 8.45983 16.3618 8.45983 16.8081 8.90615L21.9509 14.049C22.3972 14.4953 22.3972 15.2189 21.9509 15.6652C21.5046 16.1116 20.781 16.1116 20.3347 15.6652L17.1428 12.4734V22.2857C17.1428 22.9169 16.6311 23.4286 15.9999 23.4286C15.3688 23.4286 14.8571 22.9169 14.8571 22.2857V12.4734L11.6652 15.6652C11.2189 16.1116 10.4953 16.1116 10.049 15.6652C9.60265 15.2189 9.60265 14.4953 10.049 14.049L15.1918 8.90615Z"
-              fill="currentColor"></path>
-          </svg>
-        </button>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="max-w-[768px] w-full flex-1 overflow-auto">
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+    </div>
+    <div class="flex flex-col gap-1 max-w-[768px] w-full">
+      <div class="flex flex-col gap-1 p-3  bg-[#f1f1f1] rounded-2xl max-w-[768px] w-full">
+        <textarea v-model="question" class="p-2 bg-[#f1f1f1] outline-none resize-none"
+          placeholder="给DeepSeek发送消息"></textarea>
+        <div class="flex justify-end">
+          <button @click="onChat()"
+            :class="question.length === 0 ? 'text-[#f1f1f1] bg-[#cecece]' : 'text-white bg-black'"
+            class="w-8 h-8 overflow-hidden text-white bg-black rounded-full" :disabled="question.length === 0">
+            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"
+              class="icon-2xl">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M15.1918 8.90615C15.6381 8.45983 16.3618 8.45983 16.8081 8.90615L21.9509 14.049C22.3972 14.4953 22.3972 15.2189 21.9509 15.6652C21.5046 16.1116 20.781 16.1116 20.3347 15.6652L17.1428 12.4734V22.2857C17.1428 22.9169 16.6311 23.4286 15.9999 23.4286C15.3688 23.4286 14.8571 22.9169 14.8571 22.2857V12.4734L11.6652 15.6652C11.2189 16.1116 10.4953 16.1116 10.049 15.6652C9.60265 15.2189 9.60265 14.4953 10.049 14.049L15.1918 8.90615Z"
+                fill="currentColor"></path>
+            </svg>
+          </button>
+        </div>
       </div>
+      <p class="py-1 text-xs text-center text-gray-400">内容由AI生成，请注意甄别</p>
     </div>
   </div>
 </template>
@@ -21,9 +50,14 @@
 <script setup lang='ts'>
 // import axios from 'axios';
 const question = ref("");
+const chatList: {
+  role: '',
+  content: ''
+}[] = ref([])
 
-const message = ref('');
-
+const onChat = () => {
+  fetchStreamData(question.value);
+}
 // 调用示例
 // fetchStreamData("hello");
 
