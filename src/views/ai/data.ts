8fed0b0800967b55c9720548eac22d4d018d5bad
周小康 <346960620@qq.com>
2025-04-28 13:39:06 +0800

    update project

--- src/views/ai/data.ts
+++ src/views/ai/data.ts
@@ -1,15 +1,4 @@
 export const MODEL = {
-  DEEP_SEEK_V3: '',
-  DEEP_SEEK_R1: ''
-}
-
-const models = ref([
-  {
-    label: 'DeepSeek V3',
-    value: 'deepseek-chat'
-  },
-  {
-    label: 'DeepSeek R1',
-    value: 'deepseek-reasoner'
-  },
-]);
+  DEEP_SEEK_V3: "",
+  DEEP_SEEK_R1: "",
+};
